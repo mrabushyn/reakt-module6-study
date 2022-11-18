b242c53373e60f401c7c31c6404e761a99d35e88
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-11-18 19:50:40 +0200

    add proj

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { Layout } from 'components/Layout/Layout';
+import { AppBar } from 'components/AppBar/AppBar';
+import { TaskForm } from 'components/TaskForm/TaskForm';
+import { TaskList } from 'components/TaskList/TaskList';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      <TaskList />
+    </Layout>
   );
 };
+
+// В першу чергу пропишемо базові вимоги до інтерфейсу та логіки роботи програми:
+
+// Інтерфейс повинен складатися з кількох частин:
+//    Шапка з інформацією про завдання та фільтри
+//    Форма з полем введення для створення нових завдань
+//    Список завдань
+
+// У шапці необхідно відображати:
+//    Кількість виконаних та невиконаних завдань
+//    Фільтри списку завдань зі значеннями «All», «Active» та «Completed»
+
+// У кожного елементу списку завдань має бути:
+//    Абзац з текстом який вводив у форму користувач під час створення завдання
+//    Чекбокс перемикання статусу «виконано»
+//    Кнопка видалення завдання
